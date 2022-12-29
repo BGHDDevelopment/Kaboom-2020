9252c13c5eb57e6724d4ea4f2ae58fd6b0f1bec4
RyanMoodGAMING <ryan@ryanmood.com>
2022-12-29 21:35:18 +0000

    Format

--- src/main/java/com/bghddevelpment/kaboom/commands/KaboomCommand.java
+++ src/main/java/com/bghddevelpment/kaboom/commands/KaboomCommand.java
@@ -6,18 +6,9 @@ import com.bghddevelpment.kaboom.Kaboom;
 import com.bghddevelpment.kaboom.utilities.Color;
 import com.google.common.collect.ImmutableList;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 import org.bukkit.util.Vector;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @CommandAlias("kaboom|kaboom")
 @Description("Kaboom everyone.")
@@ -33,14 +24,14 @@ public class KaboomCommand extends BaseCommand {
     @Default
     public void onDefault(CommandSender sender, String[] args) {
 
-            for (Player target : Bukkit.getOnlinePlayers()) {
-                if (!target.hasPermission("kaboom.bypass")) {
-                    target.getWorld().strikeLightningEffect(target.getLocation());
-                    target.setVelocity(new Vector(0, 64, 0));
-                    target.setFallDistance(-65.0F);
-                    target.sendMessage(Color.translate(plugin.getConfig().getString("Messages.Message1")));
-                }
+        for (Player target : Bukkit.getOnlinePlayers()) {
+            if (!target.hasPermission("kaboom.bypass")) {
+                target.getWorld().strikeLightningEffect(target.getLocation());
+                target.setVelocity(new Vector(0, 64, 0));
+                target.setFallDistance(-65.0F);
+                target.sendMessage(Color.translate(plugin.getConfig().getString("Messages.Message1")));
             }
+        }
 
         return;
     }
